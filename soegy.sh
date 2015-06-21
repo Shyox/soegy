caa8c3de7f2088869ed1c4d314f8eea9d6da4a4f
Pizzarabe <robin.alster@mailbox.org>
2015-06-21 19:25:07 +0200

    more comments

--- soegy.sh
+++ soegy.sh
@@ -4,40 +4,41 @@
 
 question=1
 default=0
-
+#tempfiles
 temp1=$(mktemp)
 temp2=$(mktemp)
-
-#TEST
-temp3=$(mktemp)
-#
+#Arguments
 args=($1 $2 $3)
 
+#PATH to soegyroot, soegy.sh and qanda.csv
 soegyfullpath=$(readlink -f "$0")
 soegypath=$(dirname "$soegyfullpath")
 qanda=$soegypath/qanda.csv
-
+#
 
 #Help Argument
 if [[ $(echo ${args[@]} | grep -c "\--help" ) -eq 1 ]]
   then
+  #Display Helptext
   echo "HELP!!"
   exit
 fi
+#
 
-#Argument "-q="
+#Argument "-q=", how many questions?
 questioncount=$(echo ${args[@]} | grep -o "\-q=[0-9]\{1,9\}$" | tr -d "\-q=")
 
 if [[ -n $questioncount ]]
    then
+
   question=$questioncount
   default=1
-questioncount=$(echo ${args[@]} | grep -o "\-q=[1-65536]$" | tr -d "\-q=")
+
+#questioncount=$(echo ${args[@]} | grep -o "\-q=[1-65536]$" | tr -d "\-q=")
 fi
+#
 
-##MEHR ARGUMENTE
-
-#Initialisierung
+#show initialisierung
 if [[ $default -eq 0  ]]
    then
   echo "---Initialisierung mit DEFAULT---"
@@ -50,16 +51,19 @@ fi
 echo "Anzahl der Fragen: $question"
 echo "---------"
 echo ""
+#
+
 #csv check
-
-
 if [[ $(expr $(wc -l $qanda | cut -f 1 -d " ") \* 3) -ne $(grep -o "\;" $qanda | wc -l)  ]]
    then
   echo "FEHLER"
   exit
 fi
+#
 
-
+#Cache creation
+#DEBUG
+#Replace with user input
 command="cut -d \";\" -f 2 $qanda | grep -n \"server:web\" | cut -d \":\" -f 1"
 
 cachename=$(echo $command | md5sum | cut -d " " -f1)
@@ -77,7 +81,6 @@ echo $(md5sum $qanda | cut -d " " -f1) > $soegypath\/cache\/$cachename
 while read linematch
  do
 
-
 sed -n $(echo $linematch\p) $qanda >> $soegypath\/cache\/$cachename
 
 done < $temp1
@@ -85,42 +88,40 @@ done < $temp1
 fi
 
 rm -f $temp1
+#
 
-##DEBUG
+
+#DEBUG
 doc=example/example1.fodt
-##
+#
 
+#fodt part
+#where is the text part?
 docpre=$(grep -n "</text:sequence-decls>" $soegypath/$doc | cut -d ":" -f1)
 head -n $docpre $soegypath/$doc > $temp2
 
-###gogo
-# shuf -i
 maxline=$(wc -l  $soegypath\/cache\/$cachename | cut -d " " -f1)
 
+#Shuffle
 currentline=0
 seq 2 $maxline | shuf | while read shufline
 do
   currentline=$(expr $currentline + 1)
 
-#  echo '<text:p text:style-name="P1">TEST</text:p>' >> $temp2
-
-
+#Text into fodt
 echo -n '<text:p text:style-name="P1">' $(sed -n $(echo $shufline\p) $soegypath\/cache\/$cachename |cut -d ";" -f3 ) >> $temp2
 echo '</text:p>' >>$temp2
-
+#break if enought questions are there
   if [[ $currentline -eq $question ]]; then
     break
   fi
 done
-
-
-
+#end fodt
 echo '</office:text>' >> $temp2
 echo '</office:body>' >> $temp2
 echo '</office:document>' >> $temp2
 
-#cat $temp2
-
+#Some DEBUG
 cp $temp2 /tmp/test3.fodt
 
 rm -rf $temp2
